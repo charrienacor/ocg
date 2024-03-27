2a7994bdb3aa8a583fc5eac483a24c72c07b69fd
Yannamii <98679314+Pollyyanna@users.noreply.github.com>
2024-03-27 23:17:54 +0800

    Home page created
    - nav bar of homepage fixed
    - buttons on homepage fixed
    - first 4 pages (landing page, homepage, and the 2 login pages) are still not responsive (fit for laptop/pc only)

--- src/routes/login-student/+page.svelte
+++ src/routes/login-student/+page.svelte
@@ -1,5 +1,13 @@
 <head>
+	<link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet">
+  
 	<style>
+		header{
+			margin-top: 30px;
+			margin-left: 25px;
+			display: flex;
+			font-family : Urbanist, sans-serif;
+		}
 		.mainbody {
 			min-height: 100vh;
 			display: flex;
