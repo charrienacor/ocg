381071b43f1029652aaaf0f4ad6067304e0ad895
KaiserAce <lester.ignacio1209@gmail.com>
2024-04-11 02:05:59 +0800

    docker and makefile init

--- database/appointment.sql
+++ database/appointment.sql
@@ -1,3 +1,16 @@
+CREATE TABLE user (
+    id VARCHAR(255) PRIMARY KEY,
+    google_id VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255)
+);
+
+CREATE TABLE user_session (
+    id VARCHAR(255) PRIMARY KEY,
+    expires_at DATETIME NOT NULL,
+    user_id VARCHAR(255) NOT NULL REFERENCES user(id)
+);
+
 CREATE TABLE Appointments(
     Appointment_ID INTEGER(12) NOT NULL UNIQUE,
     Student_Name VARCHAR(100) NOT NULL,
--- database/init.sql
+++ database/init.sql
@@ -2,8 +2,5 @@ DROP DATABASE IF EXISTS PatnubayAguhon;
 CREATE DATABASE PatnubayAguhon;
 USE PatnubayAguhon;
 
-SOURCE database/counselors.sql
-SOURCE database/appointment.sql
-SOURCE database/auth.sql
 
 
