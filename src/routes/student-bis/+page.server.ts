5b0389edbbc06052dcf9557ce361ae93a5fb70a7
KaiserAce <lester.ignacio1209@gmail.com>
2024-04-17 01:01:55 +0800

    BIS Progress

---
+++ src/routes/student-bis/+page.server.ts
@@ -0,0 +1,23 @@
+import { redirect, type RequestHandler } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
+import { fail } from "@sveltejs/kit";
+import { superValidate } from "sveltekit-superforms";
+import { formSchema } from "./schema";
+import { zod } from "sveltekit-superforms/adapters";
+import db from "$lib/server/database";
+
+
+export const actions: Actions = {
+    default: async (event) => {
+      const form = await superValidate(event, zod(formSchema));
+      if (!form.valid) {
+        return fail(400, {
+          form,
+        });
+      }
+      console.log(form.data);
+      return {
+        form,
+      };
+    },
+  };
