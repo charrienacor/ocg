bf7216b3ccb9334013a0ab439dbfbfbfe6a9d704
Yannamii <98679314+Pollyyanna@users.noreply.github.com>
2024-04-27 20:33:45 +0800

    Tables on Admin side converted to Shadcn

--- src/routes/admin/records/edit/data-table.svelte
+++ src/routes/admin/records/edit/data-table.svelte
@@ -14,22 +14,13 @@
     import { readable } from "svelte/store";
     import DataTableActions from "./data-table-actions.svelte";
     import ArrowUpDown from "lucide-svelte/icons/arrow-up-down";
-    import ChevronDown from "lucide-svelte/icons/chevron-down";
     import * as Table from "$lib/components/ui/table/index.js";
     import { Button } from "$lib/components/ui/button/index.js";
-    import Check from "lucide-svelte/icons/check";
-    import * as Command from "$lib/components/ui/command/index.js";
-    import * as Popover from "$lib/components/ui/popover/index.js";
     import { cn } from "$lib/utils.js";
-    import { tick } from "svelte";
-
-    import CalendarIcon from "lucide-svelte/icons/calendar";
     import {
         DateFormatter,
         type DateValue,
-        getLocalTimeZone
     } from "@internationalized/date";
-    import { Calendar } from "$lib/components/ui/calendar/index.js";
     
     const df = new DateFormatter("en-US", {
         dateStyle: "long"
@@ -38,7 +29,7 @@
     let value: DateValue | undefined = undefined;
    
     type Payment = {
-      name: string;
+      id: string;
       date: any;
       time: any;
       nature: string;
@@ -47,35 +38,35 @@
    
     const data: Payment[] = [
       {
-        name: "Kim Sunoo",
+        id: "wqerq234r",
         date: "April 25, 2024",
         time: "10:30 AM",
         nature: "Academic Stress",
         status: "Accepted"
       },
       {
-        name: "Park Jongseong",
+        id: "345hge",
         date: "April 26, 2024",
         time: "2:30 PM",
         nature: "Heartbroken",
         status: "Rejected"
       },
       {
-        name: "Lee Heeseung",
+        id: "tr32t34fr",
         date: "April 27, 2024",
         time: "1:30 PM",
         nature: "Ghosted",
         status: "Rejected"
       },
       {
-        name: "Nishimura Riki",
+        id: "tq34frw34",
         date: "April 28, 2024",
         time: "10:30 AM",
         nature: "Family Pressure",
         status: "Accepted"
       },
       {
-        name: "Yang Jungwon",
+        id: "t314rf3",
         date: "April 29, 2024",
         time: "8:30 AM",
         nature: "Career Stress",
@@ -94,8 +85,8 @@
    
     const columns = table.createColumns([
       table.column({
-        header: "Name",
-        accessor: "name",
+        header: "Appointment ID",
+        accessor: "id",
         plugins: { sort: { disable: true }, filter: { exclude: true } }
       }),
       table.column({
@@ -122,7 +113,7 @@
       }),
       table.column({
         accessor: ({ id }) => id,
-        header: "Status",
+        header: "Remarks",
         cell: ({ value }) => {
           return createRender(DataTableActions, { id: value });
         },
@@ -140,101 +131,13 @@
    
     const { sortKeys } = pluginStates.sort;
    
-    const { hiddenColumnIds } = pluginStates.hide;
     const ids = flatColumns.map((c) => c.id);
-    let hideForId = Object.fromEntries(ids.map((id) => [id, true]));
-   
-    $: $hiddenColumnIds = Object.entries(hideForId)
-      .filter(([, hide]) => !hide)
-      .map(([id]) => id);
    
     const { selectedDataIds } = pluginStates.select;
-   
-    const frameworks = [
-        {
-        user: "student",
-        label: "Student",
-        },
-        {
-        user: "visitor",
-        label: "Visitor",
-        }
-    ];
-    
-    let open = false;
-    let user = "";
 
-    $: selectedValue =
-        frameworks.find((f) => f.user === user)?.label ??
-        "User";
-    
-    function closeAndFocusTrigger(triggerId: string) {
-        open = false;
-        tick().then(() => {
-        document.getElementById(triggerId)?.focus();
-        });
-    }
   </script>
    
   <div class="w-full">
-    <div class="flex items-center py-4">
-        <Popover.Root>
-            <Popover.Trigger asChild let:builder>
-              <Button
-                variant="outline"
-                class={cn(
-                  "w-[280px] justify-start text-left font-normal",
-                  !value && "text-muted-foreground"
-                )}
-                builders={[builder]}
-              >
-                <CalendarIcon class="mr-2 h-4 w-4" />
-                {value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date"}
-              </Button>
-            </Popover.Trigger>
-            <Popover.Content class="w-auto p-0">
-              <Calendar bind:value initialFocus />
-            </Popover.Content>
-          </Popover.Root>
-          
-      <Popover.Root bind:open let:ids>
-        <Popover.Trigger asChild let:builder>
-          <Button
-            builders={[builder]}
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            class="w-[150px] justify-between ml-auto"
-          >
-            {selectedValue}
-            <ChevronDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </Popover.Trigger>
-        <Popover.Content class="w-[150px] p-0">
-          <Command.Root>
-            <Command.Group>
-              {#each frameworks as framework}
-                <Command.Item
-                  user={framework.user}
-                  onSelect={(currentUser) => {
-                    user = currentUser;
-                    closeAndFocusTrigger(ids.trigger);
-                  }}
-                >
-                  <Check
-                    class={cn(
-                      "mr-2 h-4 w-4",
-                      user !== framework.user && "text-transparent"
-                    )}
-                  />
-                  {framework.label}
-                </Command.Item>
-              {/each}
-            </Command.Group>
-          </Command.Root>
-        </Popover.Content>
-      </Popover.Root>
-    </div>
     <div class="rounded-md border">
       <Table.Root {...$tableAttrs}>
         <Table.Header>
@@ -252,23 +155,7 @@
                       {...attrs}
                       class={cn("[&:has([role=checkbox])]:pl-3")}
                     >
-                      {#if cell.id === "amount"}
-                        <div class="text-right font-medium">
-                          <Render of={cell.render()} />
-                        </div>
-                      {:else if cell.id === "email"}
-                        <Button variant="ghost" on:click={props.sort.toggle}>
-                          <Render of={cell.render()} />
-                          <ArrowUpDown
-                            class={cn(
-                              $sortKeys[0]?.id === cell.id && "text-foreground",
-                              "ml-2 h-4 w-4"
-                            )}
-                          />
-                        </Button>
-                      {:else}
                         <Render of={cell.render()} />
-                      {/if}
                     </Table.Head>
                   </Subscribe>
                 {/each}
