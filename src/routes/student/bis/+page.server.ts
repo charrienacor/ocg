a2c931578baec11556b3fb3196418f77dec3c9c9
Charrie Anne S. Nacor <111645489+charrienacor@users.noreply.github.com>
2024-04-28 22:52:47 +0800

    updated +page.server.ts

--- src/routes/student/bis/+page.server.ts
+++ src/routes/student/bis/+page.server.ts
@@ -1,9 +1,20 @@
-import type { Actions} from "./$types";
+import { redirect, type RequestHandler } from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
 import { fail } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms";
 import { formSchema } from "./schema";
 import { zod } from "sveltekit-superforms/adapters";
 import db from "$db/mongo";
+
+export const load: PageServerLoad = async (event) => {
+  if (!event.locals.user) redirect(302, "/student/login");
+
+  return {
+    name: event.locals.user.username,
+    email: event.locals.user.email,
+    form: await superValidate(zod(formSchema)),
+  };
+};
 
 export const actions: Actions = {
   default: async (event) => {
@@ -13,7 +24,7 @@ export const actions: Actions = {
         form,
       });
     }
-    
+  
     const data = form.data;
 
     await db.collection("BIS").insertOne({
