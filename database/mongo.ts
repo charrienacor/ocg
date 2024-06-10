7eec1921efa6febab9b3a77fb3c7668cef5356b2
KaiserAce <lester.ignacio1209@gmail.com>
2024-06-11 00:52:25 +0800

    production config in progress

--- database/mongo.ts
+++ database/mongo.ts
@@ -1,5 +1,15 @@
 import { MongoClient } from "mongodb";
-
-const client = new MongoClient("mongodb://host.docker.internal:27017");
+import { dev } from "$app/environment";
+import {
+  MONGODB_LOCAL,
+  MONGODB_PRODUCTION,
+} from "$env/static/private";
+let connection_string = "";
+if (!dev) {
+  connection_string = MONGODB_PRODUCTION;
+} else {
+  connection_string = MONGODB_LOCAL;
+}
+const client = new MongoClient(connection_string);
 await client.connect();
 export default client.db("Aguhon");
