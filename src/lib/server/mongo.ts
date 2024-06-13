ce87055ff0d10b21a4f8d514146f2db17bbd5fef
KaiserAce <lester.ignacio1209@gmail.com>
2024-06-13 16:32:52 +0800

    test

--- src/lib/server/mongo.ts
+++ src/lib/server/mongo.ts
@@ -3,7 +3,11 @@ import { dev } from "$app/environment";
 import { env } from "$env/dynamic/private";
 let connection_string = "";
 if (!dev) {
-  connection_string = `mongodb://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOSTNAME}:${env.DATABASE_PORT}`;
+  if (env.DATABASE_HOSTNAME === undefined) {
+    connection_string = "mongodb://localhost:27107"
+  } else {
+    connection_string = `mongodb://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOSTNAME}:${env.DATABASE_PORT}`;
+  }
 } else {
   connection_string = `mongodb://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@localhost:${env.DATABASE_PORT}`;
 }
